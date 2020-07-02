b11b7e3a5023be6364b708465aa638d3ade2a7a7
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-02 18:28:24 +0200

    cookie domain

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000, // Session is valid for 24 hours
   keys: [env.session.COOKIE_KEY],
   // httpOnly: true, /** true is default **/
-  domain: env.COOKIE_DOMAIN, 
+  domain: env.BASE_API_URL, 
   // secure: true,
   // name: 'izypaper',
   sameSite: 'none',
