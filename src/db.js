422378cf8f52c4fa306c86808e8d36ce9c43cff5
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-14 18:32:19 +0200

    mongoose

--- src/db.js
+++ src/db.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 import { env } from './config';
 
-mongoose.connect(env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  mongoose.connection.db.dropDatabase();
-});
+mongoose.connect(
+  env.MONGODB_URL, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, 
+  () => mongoose.connection.db.dropDatabase()
+);
 export const db = mongoose.connection;
 
 db.on('error', error => console.log('MONGO ERROR', error));
