2f8e969906b583c3b32ee0c2bb9d3d04e1a51c31
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-17 23:27:02 +0200

    email accounts on startup

--- src/db.js
+++ src/db.js
@@ -5,17 +5,17 @@ import { User } from './models';
 const users = [
   {
     email: 'mickael@izypaper.com',
-    password: 'mickael',
+    password: '$2a$10$mvI8mj6w5sWtKRQ23IqG3OMkRsvFrtgh2Ut61icuEAMeSCIDjDY/6',
     roles: ['ADMIN']
   },
   {
     email: 'cecile@izypaper.com',
-    password: 'cecile',
+    password: '$2a$10$j6h.xwGMICOpaCfcAdilj.OsgHvLAcsQEdig2i87yorGBaFBsbZPe',
     roles: ['ADMIN']
   },
   {
     email: 'malo@izypaper.com',
-    password: 'malo',
+    password: '$2a$10$jnhs4.1DKl5gS5jaW0wKhusl0XRCqaAXBMDzAiEDqBYEphlH8lJNe',
     roles: ['ADMIN']
   }
 ]
@@ -32,7 +32,6 @@ db.on('error', error => console.log('MONGO ERROR', error));
 db.on('open', () => {
   console.log(`[MONGODB] : Connection OK`);
   users.map(user => {
-    console.log('user', user);
     User.create(user);
   });
 });
