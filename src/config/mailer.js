76772a65a800d4d3cd87d72cdc204e4e9149739b
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-18 15:42:19 +0200

    nodemailer env

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,10 +1,10 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  host: 'pro2.mail.ovh.net',
-  port: 587,
-  auth: {
-    user: 'mickael@izypaper.com',
-    pass: '*Xyu*Nmk8@W$ptY'
-  }
+	host: 'pro2.mail.ovh.net',
+	port: 587,
+	auth: {
+		user: 'mickael@izypaper.com',
+		pass: process.env.NODEMAILER_EMAIL_ACCOUNT_PASSWORD
+	}
 });
