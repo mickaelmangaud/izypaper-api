6d7eea10bae0f6cb33d4a524a9331b967c063e14
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-27 16:43:32 +0200

    rebuild

--- src/index.js
+++ src/index.js
@@ -77,8 +77,6 @@ app.use(notFoundHandler);
 /* Custom error handler */
 app.use(errorHandler);
 
-console.log('ahahaha')
-
 app.listen({ port: env.PORT }, () => {
   logger.info(`Server ready at ${env.BASE_API_URL}/graphql`);
 });
